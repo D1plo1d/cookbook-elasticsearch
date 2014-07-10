c287f659a2ab9727d87323a9676cca311bf65d31
Rob Gilson <thatotherdude@gmail.com>
2014-07-10 15:41:32 -0400

    long description was breaking opsworks

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,6 @@ maintainer       "karmi"
 maintainer_email "karmi@karmi.cz"
 license          "MIT License"
 description      "Installs and configures elasticsearch on Amazon EC2"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.markdown'))
 version          "0.0.1"
 
 recommends 'java'
