158b3a67ed9479c3115ac42680e7233b08c75677
D1plo1d <d1plo1d@d1plo1d.com>
2014-08-12 17:34:52 -0400

    Defaulting min mem to equal max mem as per best practices.

--- attributes/default.rb
+++ attributes/default.rb
@@ -20,8 +20,10 @@ default.elasticsearch[:pid_path]  = "/usr/local/var/run/elasticsearch"
 # Maximum amount of memory to use is automatically computed as 2/3 of total available memory.
 # You may choose to configure it in your node configuration instead.
 #
+# Setting min_mem to false will cause the min_mem to equal the max_mem.
+#
 max_mem = "#{(node.memory.total.to_i - (node.memory.total.to_i/3) ) / 1024}m"
-default.elasticsearch[:min_mem] = "128m"
+default.elasticsearch[:min_mem] = false
 default.elasticsearch[:max_mem] = max_mem
 
 # === LIMITS ===
