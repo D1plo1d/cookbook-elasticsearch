33b59212061c5be50045434180c2a6c492c19ab2
D1plo1d <d1plo1d@d1plo1d.com>
2014-08-14 12:48:18 -0400

    Setting heap memory to 1/2 of total memory by default. As per http://asquera.de/opensource/2012/11/25/elasticsearch-pre-flight-checklist/

--- attributes/default.rb
+++ attributes/default.rb
@@ -17,12 +17,12 @@ default.elasticsearch[:pid_path]  = "/usr/local/var/run/elasticsearch"
 
 # === MEMORY ===
 #
-# Maximum amount of memory to use is automatically computed as 2/3 of total available memory.
+# Maximum amount of memory to use is automatically computed as 1/2 of total available memory.
 # You may choose to configure it in your node configuration instead.
 #
 # Setting min_mem to false will cause the min_mem to equal the max_mem.
 #
-max_mem = "#{(node.memory.total.to_i - (node.memory.total.to_i/3) ) / 1024}m"
+max_mem = "#{(node.memory.total.to_i/2) / 1024}m"
 default.elasticsearch[:min_mem] = false
 default.elasticsearch[:max_mem] = max_mem
 
